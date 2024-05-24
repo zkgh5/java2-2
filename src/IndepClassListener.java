f1e7e2091a921a2d5b68e6a65753939feae49392
zkgh5 <rose55229@NAVER.COM>
2024-05-24 16:46:52 +0900

    5월 24일 정리본

---
+++ src/IndepClassListener.java
@@ -0,0 +1,26 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class IndepClassListener extends JFrame {
+    public IndepClassListener() {
+        setTitle("Action 이벤트 리스너 예제");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c = getContentPane();
+        c.setLayout(new FlowLayout());
+        JButton btn = new JButton();
+        btn.addActionListener(new MyActionListener());
+        c.add(btn);
+        setSize(250, 120);
+        setVisible(true);
+
+        public static void main(String [] args) {
+            new IndepClassListener();
+        }
+    }
+}
+
+    class MyActionListener implements ActionListener {
+        
+    } 
